5ed367309bbff31bce885f3a957b122d8867dc16
Cai heng <ianyaheng822@gmail.com>
2020-04-23 16:33:44 +1000

    add the data to cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react'
 import {Cards, Charts, CountryPicker} from './components'
-import style from './App.module.css'
+import styles from './App.module.css'
 import {fetchData} from './api/index'
 
 
 class App extends Component {
+  state ={
+    data:{},
+  }
 
   async componentDidMount(){
-    const data= await fetchData();
-    console.log(data);
-    
+    const fetchedData= await fetchData();
+    this.setState({data:fetchedData})
+  
   }
   render() {
     return (
-      <div className={style.container}>
-      <Cards/>
+    <div className={styles.container}>
+      <Cards data={this.state.data}/>
       <Charts/>
       <CountryPicker/>
     </div>
