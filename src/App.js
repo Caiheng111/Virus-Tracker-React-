c21c6fd39be38faf3c5e61d793de8a014e1c272f
Cai heng <ianyaheng822@gmail.com>
2020-04-23 15:26:15 +1000

    add the APi

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react'
 import {Cards, Charts, CountryPicker} from './components'
+import style from './App.module.css'
+import {fetchData} from './api/index'
 
-import './App.css';
 
-function App() {
-  return (
-    <div>
+class App extends Component {
 
+  async componentDidMount(){
+    const data= await fetchData();
+    console.log(data);
+    
+  }
+  render() {
+    return (
+      <div className={style.container}>
       <Cards/>
       <Charts/>
       <CountryPicker/>
-    
     </div>
-  );
+    )
+  }
 }
 
 export default App;
+
+
+
+
+
+
+
